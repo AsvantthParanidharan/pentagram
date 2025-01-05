6a0ba45ca2d6a6cae61947a426ec39f7ddb3ee41
Asvantth Paranidharan <asvantth@gmail.com>
2025-01-05 16:54:15 -0600

    commit to github

--- src/app/api/generate-image/route.ts
+++ src/app/api/generate-image/route.ts
@@ -1,4 +1,7 @@
 import { NextResponse } from "next/server";
+import { put } from "@vercel/blob";
+import crypto from "crypto";
+import { blob } from "stream/consumers";
 
 export async function POST(request: Request) {
   try {
@@ -7,10 +10,41 @@ export async function POST(request: Request) {
 
     // TODO: Call your Image Generation API here
     // For now, we'll just echo back the text
+    const modal_generate_url = process.env.MODAL_GENERATE_URL;
+    const url = new URL(modal_generate_url || "");
+
+    url.searchParams.set("prompt", text);
+
+    console.log("Requesting URL", url.toString());
+
+    const response = await fetch(url.toString(), {
+      method: "GET",
+      headers: {
+        "X-API-KEY": process.env.API_KEY || "",
+        Accept: "image/jpeg",
+      },
+    });
+
+    if (!response.ok) {
+      const errorText = await response.text();
+      console.error("API Reponse:", errorText);
+      throw new Error(
+        `HTTP error! statusL ${response.status}, message: ${errorText}`
+      );
+    }
+
+    const imageBuffer = await response.arrayBuffer();
+
+    const filename = `${crypto.randomUUID()}.jpg`;
+
+    const blob = await put(filename, imageBuffer, {
+      access: "public",
+      contentType: "image/jpeg",
+    });
 
     return NextResponse.json({
       success: true,
-      message: `Received: ${text}`,
+      imageUrl: blob.url,
     });
   } catch (error) {
     return NextResponse.json(
